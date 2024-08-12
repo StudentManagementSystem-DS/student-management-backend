35cd69c75ec5adf87c8d50d6d64aac117a7b7ef5
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-12 20:21:08 +0300

    Initial commit with project files

---
+++ src/test/java/com/dsvetiev/studentmanagement/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsvetiev.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
