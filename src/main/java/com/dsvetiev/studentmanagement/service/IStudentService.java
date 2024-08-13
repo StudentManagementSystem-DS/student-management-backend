81697742a7f19eaf591f0b147dacf8f9a0926d0b
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-13 17:59:02 +0300

    add: IStudentservice interface with CRUD methods for Student entity

---
+++ src/main/java/com/dsvetiev/studentmanagement/service/IStudentService.java
@@ -0,0 +1,15 @@
+package com.dsvetiev.studentmanagement.service;
+
+import com.dsvetiev.studentmanagement.model.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+
+    Student addStudent(Student student);
+    List<Student> getStudents();
+    Student updateStudents(Student student, Long id);
+    Student getStudentId(Long id);
+    void deleteStudent(Long id);
+
+}
